4e65fc01eef8fde105ee823cd294f1426bb6b695
jiangxiuqiang <965289186@qq.com>
2018-12-08 21:00:59 +0800

    解决乱码问题

--- src/main/java/demo/xml/sax/SaxTest.java
+++ src/main/java/demo/xml/sax/SaxTest.java
@@ -1,13 +1,13 @@
 package demo.xml.sax;
 
 import java.io.FileInputStream;
+import java.io.InputStreamReader;
 import java.util.List;
 
-import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
 
-import org.xml.sax.SAXException;
+import org.xml.sax.InputSource;
 
 import demo.xml.entity.Book;
 
@@ -29,9 +29,7 @@ public class SaxTest {
 	/**
 	 * sax解析xml
 	 * 
-	 * @return
-	 * @throws Exception 
-	 * @throws  
+	 * @return @throws Exception @throws
 	 */
 	public static List<Book> saxParseXml() throws Exception {
 		// 获取SAXParserFactory对象
@@ -39,7 +37,11 @@ public class SaxTest {
 		// 获取SAXParser对象
 		SAXParser saxParser = saxParserFactory.newSAXParser();
 		SaxParseHandler saxParseHandler = new SaxParseHandler();
-		saxParser.parse("src/main/resources/books.xml", saxParseHandler);
+//		saxParser.parse("src/main/resources/books.xml", saxParseHandler);
+		FileInputStream fs = new FileInputStream("src/main/resources/books.xml");
+		// 解决乱码问题
+		InputStreamReader isr = new InputStreamReader(fs,"UTF-8");
+		saxParser.parse(new InputSource(isr), saxParseHandler);
 		return saxParseHandler.getBookList();
 	}
 
