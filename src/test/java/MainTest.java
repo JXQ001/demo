ce62b4b0207cd6601f9dd89ba002a9df3a0de5bc
jiangxiuqiang <965289186@qq.com>
2018-12-14 15:01:02 +0800

    test IDEA commit.

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,13 @@
+/**
+ * @author jiangxiuqiang
+ * @version 1.0
+ * @ClassName:MainTest
+ * @Description:
+ * @date 2018/12/14 14:58
+ */
+public class MainTest {
+
+    public static void main(String[] args) {
+        System.out.println("This is a test class.");
+    }
+}
