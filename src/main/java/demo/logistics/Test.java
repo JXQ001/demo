65c02e13cf89b85e60173255997fc42834a4b574
jiangxiuqiang <965289186@qq.com>
2019-01-09 20:30:24 +0800

    本地修改

--- src/main/java/demo/logistics/Test.java
+++ src/main/java/demo/logistics/Test.java
@@ -6,13 +6,19 @@ public class Test {
 
 		String logistics = "demo.logistics.ShunFeng";
 		logistics = "demo.logistics.ZhongTong";
-		Class clazz = Class.forName(logistics);
+		Class<?> clazz = Class.forName(logistics);
 		Logistics obj = (Logistics) clazz.newInstance();
 
 		Context context = new Context(obj);
 		context.zaiXianYuYue("北京张三", "上海李四");
 		context.dianZiMianDan("北京张三", "上海李四");
 		context.chaXunWuLiuGuiJi("1102322");
+		
+		test();
+	}
+
+	private static void test() {
+		System.out.println("local mod");
 	}
 
 }
