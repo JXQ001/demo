e7f262f1f0d9b20bdc68b8527ba707cb80c03059
jiangxiuqiang <965289186@qq.com>
2019-05-26 20:08:16 +0800

    总结

--- src/main/java/demo/interview/Main.java
+++ src/main/java/demo/interview/Main.java
@@ -1,5 +1,11 @@
 package demo.interview;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
+
+import org.eclipse.jetty.util.thread.ExecutorThreadPool;
+
 /**
  * @author jiangxiuqiang
  * @version 1.0
@@ -9,6 +15,8 @@ package demo.interview;
  */
 public class Main {
     public static void main(String[] args) {
-
+    	ExecutorService fixedThreadPool = Executors.newFixedThreadPool(5);
+    	ExecutorService singleThreadExecutor = Executors.newSingleThreadExecutor();
+    	ExecutorService chedThreadPool = Executors.newCachedThreadPool();
     }
 }
